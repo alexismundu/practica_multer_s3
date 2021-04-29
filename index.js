2ae0cfb05ef6f9d708e42f69310dd10d8a026ee8
Alexis Munoz <alexis.munoz@wizeline.com>
2021-04-29 09:07:56 -0500

    Get all app working

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config()
 const aws = require("aws-sdk");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
-
-
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWSAccessKeyId,
-    secretAccessKey: process.env.AWSSecretKey
-});
-
 const app = express()
 const port = 3000
 
 aws.config.update({
     secretAccessKey: process.env.AWSSecretKey,
     accessKeyId: process.env.AWSAccessKeyId,
-    region: 'us-east-1'
+    region: process.env.AWS_REGION
 });
 
+const s3 = new aws.S3();
+app.use(bodyParser.json());
 
 var upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: 'hhhcouk',
+        bucket: 'testbucket20210429',
+        acl: 'public-read',
         key: function (req, file, cb) {
             console.log(file);
             cb(null, file.originalname); //use Date.now() for unique file keys
         }
     })
 });
-//Const s3 = new aws.S3();
-app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 })
+
+app.post('/upload', upload.single('img'), function (req, res) {
+    const info = req.body;
+    const image = req.file;
+    console.log(info, image)
+    res.send("Uploaded!");
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
